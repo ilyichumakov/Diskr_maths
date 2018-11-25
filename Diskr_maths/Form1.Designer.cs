5e8b5c8686e34ab62551e5e3b018822eaa922c9c
ilyichumakov <44948620+ilyichumakov@users.noreply.github.com>
2018-11-25 15:23:58 +0300

    That works

--- Diskr_maths/Form1.Designer.cs
+++ Diskr_maths/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(335, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить вопрос";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(335, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Запустить расчёт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 451);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Diskr_maths/Form1.cs
+++ Diskr_maths/Form1.cs
@@ -16,5 +16,43 @@ namespace Diskr_maths
         {
             InitializeComponent();
         }
+
+        private bool IsEnough()
+        {
+            int k = 0;
+            if (Form1.ActiveForm.Controls.Count > 0)
+            {                
+                foreach (Control a in Form1.ActiveForm.Controls)
+                {
+                    //MessageBox.Show(a.GetType().ToString());
+                    if(a is Label)
+                    {
+                        k++;
+                    }
+                }
+            }
+            if (k > 1) return true;
+            else return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IsEnough();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label a = new Label();
+            a.Text = "New";
+            int x = 10;
+            int y = 10;
+            while (GetChildAtPoint( new Point(x,y)) is Label)
+            {
+                y += 15;
+            }
+            a.Location = new Point(x, y);
+            Form1.ActiveForm.Controls.Add(a);            
+            if(IsEnough()) button2.Enabled = true;
+        }
     }
 }
