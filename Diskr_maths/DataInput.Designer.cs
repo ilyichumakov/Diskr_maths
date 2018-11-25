e4aaf3c8385acf026fb277daf797d0914dc409b5
Ilya Chumakov <ilya.chumakov99@gmail.com>
2018-11-25 20:15:27 +0300

    Condition for adding fields

--- Diskr_maths/DataInput.Designer.cs
+++ Diskr_maths/DataInput.Designer.cs
@@ -29,30 +29,21 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(128, 415);
+            this.button1.Location = new System.Drawing.Point(118, 639);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 19;
-            this.listBox1.Location = new System.Drawing.Point(118, 27);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(180, 23);
-            this.listBox1.TabIndex = 1;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -71,6 +62,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(141, 56);
             this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
@@ -82,20 +74,31 @@
             this.label2.Size = new System.Drawing.Size(137, 19);
             this.label2.TabIndex = 4;
             this.label2.Text = "Вариант ответа 1";
+            this.label2.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(157, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // DataInput
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(310, 450);
+            this.ClientSize = new System.Drawing.Size(310, 674);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "DataInput";
             this.ShowIcon = false;
             this.Text = "Ввод данных";
+            this.Load += new System.EventHandler(this.DataInput_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,9 +107,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Diskr_maths/DataInput.cs
+++ Diskr_maths/DataInput.cs
@@ -19,27 +19,90 @@ namespace Diskr_maths
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            Label l = new Label();
-            TextBox t = new TextBox();
-            t.Multiline = true;            
-            int x = 12;
-            int xt = 157;
-            int y = 90;
-            int k = 1;
-            t.Size = new System.Drawing.Size(141, 56);
-            while (GetChildAtPoint(new Point(xt+5, y+2)) is TextBox)
-            {
-                y += 67;
-                k++;
+            if (!IsEmptyInput()) {
+                Label l = new Label();
+                TextBox t = new TextBox();
+                t.Multiline = true;
+                int x = 12;
+                int xt = 157;
+                int y = 90;
+                int k = 1;
+                t.Size = new System.Drawing.Size(141, 56);
+                while (GetChildAtPoint(new Point(xt + 5, y + 2)) is TextBox)
+                {
+                    y += 67;
+                    k++;
+                }
+                l.Text = "Вариант ответа " + k.ToString();
+                l.Font = new Font("Tahoma", 12);
+                l.AutoSize = true;
+                l.Location = new Point(x, y + 19);
+                t.Location = new Point(xt, y);
+                t.TextChanged += new System.EventHandler(textBox1_TextChanged);
+                DataInput.ActiveForm.Controls.Add(l);
+                DataInput.ActiveForm.Controls.Add(t);
             }
-            l.Text = "Вариант ответа " + k.ToString();
-            l.Font = new Font("Tahoma", 12);
-            l.AutoSize = true;
-            l.Location = new Point(x, y+19);
-            t.Location = new Point(xt, y);
-            t.TextChanged += new System.EventHandler(textBox1_TextChanged);
-            DataInput.ActiveForm.Controls.Add(l);
-            DataInput.ActiveForm.Controls.Add(t);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label a = new Label();
+            TextBox t = new TextBox();
+            a.Text = "New";
+            a.BackColor = Color.Transparent;
+            a.AutoSize = true;
+            t.Font = new Font("Tahoma", 12);
+            t.Size = new System.Drawing.Size(200, 18);
+            int x = 10;
+            int y = 10;
+            while (GetChildAtPoint(new Point(x, y)) is Label)
+            {
+                y += 50;
+            }
+            a.Location = new Point(x, y);
+            t.Location = new Point(x, y + 17);
+            Form1.ActiveForm.Controls.Add(a);
+            Form1.ActiveForm.Controls.Add(t);
+        }
+
+        private void DataInput_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Да/нет");
+            comboBox1.Items.Add("Один вариант ответа");
+            comboBox1.Items.Add("Несколько вариантов ответа");
+            comboBox1.SelectedItem = comboBox1.Items[0];
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!(comboBox1.SelectedItem == comboBox1.Items[0]))
+            {
+                label2.Visible = true;
+                textBox1.Visible = true;
+            }
+            else
+            {
+                label2.Visible = false;
+                textBox1.Visible = false;
+            }
+        }
+
+        private bool IsEmptyInput()
+        {
+            if (Form1.ActiveForm.Controls.Count > 0)
+            {
+                foreach (Control a in DataInput.ActiveForm.Controls)
+                {
+                    //MessageBox.Show(a.GetType().ToString());
+                    if (a is TextBox)
+                    {
+                        if (a.Text == "") return true;
+                    }
+                }
+                return false;
+            }
+            else return false;
         }
     }
 }
